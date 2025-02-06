b449c2227936b156b4f40a3c83e35afc44d1e265
shaneferrellwv <shaneferrell@ufl.edu>
2025-02-06 11:25:25 -0500

    setup basic structure for creating dyanmic ranking

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { createDynamicRanking } from "./lib/dynamic-ranking";
 
-try
-{
+try {
     createDynamicRanking({
-        containerId: 'ranking'
+        containerId: 'ranking',
+        dataSourceType: 'JSON',
+        dataSource: './data.json',
+        model: 'WSM',
+        normalization: 'linear',
+        criteriaColumns: [1, 2, 3],
+        defaultWeights: [2, 3, 4]
     });
 
 }
