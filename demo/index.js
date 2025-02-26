331f9e07bc9f1e9d086658739b208d1a14c60e6e
volyc <volstadc923@gmail.com>
2025-02-26 12:16:53 -0500

    Added sliders, styling, using data

--- demo/index.js
+++ demo/index.js
@@ -1,4 +1,4 @@
-import { createDynamicRanking } from "./lib/dynamic-ranking";
+import { createDynamicRanking } from "../lib/dynamic-ranking";
 
 try {
     createDynamicRanking({
@@ -7,7 +7,7 @@ try {
         dataSource: 'https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=y&season=2024&season1=1871&startdate=&enddate=&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=0&postseason=&sortdir=default&sortstat=SO',
         model: 'WSM',
         normalization: 'linear',
-        criteriaColumns: ["Age", "IP", "ERA"],
+        criteriaColumns: ["IP", "K%", "BB%"],
         defaultWeights: [2, 3, 4]
     });
 
