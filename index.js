c14ab88702345bcde6df8fb1f9974a6bea17ed75
volyc <volstadc923@gmail.com>
2025-02-26 11:16:13 -0500

    Data link

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ try {
     createDynamicRanking({
         containerId: 'ranking',
         dataSourceType: 'JSON',
-        dataSource: './data.json',
+        dataSource: 'https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=y&season=2024&season1=1871&startdate=&enddate=&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=0&postseason=&sortdir=default&sortstat=SO',
         model: 'WSM',
         normalization: 'linear',
-        criteriaColumns: ["criteria0", "criteria1", "criteria2"],
+        criteriaColumns: ["Age", "IP", "ERA"],
         defaultWeights: [2, 3, 4]
     });
 
