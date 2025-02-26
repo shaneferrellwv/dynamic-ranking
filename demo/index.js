c77db7ce6aadea560a79f40aaf6430ecb1ac63a6
volyc <volstadc923@gmail.com>
2025-02-26 15:12:30 -0500

    Slider implementation, adjusting weights, recalulating scores, styling

--- demo/index.js
+++ demo/index.js
@@ -7,7 +7,7 @@ try {
         dataSource: 'https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=y&season=2024&season1=1871&startdate=&enddate=&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=0&postseason=&sortdir=default&sortstat=SO',
         model: 'WSM',
         normalization: 'linear',
-        criteriaColumns: ["IP", "K%", "BB%"],
+        criteriaColumns: ["Age", "K%", "IP"],
         defaultWeights: [2, 3, 4]
     });
 
