8544456d198cdc535aa87042d4992f5c69e88088
shaneferrellwv <shaneferrell@ufl.edu>
2025-02-27 16:07:05 -0500

    move responsibility of data retrieval from library to implementation

--- demo/index.js
+++ demo/index.js
@@ -1,10 +1,19 @@
+import { fetchData } from "../lib/data-fetch.js";
 import { createDynamicRanking } from "../lib/dynamic-ranking";
 
 try {
-    createDynamicRanking({
+    // get data for rankings
+    const dataSource = 'https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=y&season=2024&season1=1871&startdate=&enddate=&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=0&postseason=&sortdir=default&sortstat=SO';
+    var rawData = await fetchData(dataSource);
+
+    // manipulate/parse data from file as necessary
+    rawData = rawData.data;
+
+    // construct ranking system
+    createDynamicRanking(
+        rawData,
+        {
         containerId: 'ranking',
-        dataSourceType: 'JSON',
-        dataSource: 'https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=y&season=2024&season1=1871&startdate=&enddate=&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=0&postseason=&sortdir=default&sortstat=SO',
         model: 'WSM',
         normalization: 'linear',
         criteriaColumns: ["Age", "K%", "IP"],
