c6ab658d6df5bc7f5ae890c10333e66a592a5e50
shaneferrellwv <shaneferrell@ufl.edu>
2025-03-13 19:31:03 -0400

    wrap public creator function into public class constructor

--- demo/index.js
+++ demo/index.js
@@ -1,22 +1,25 @@
 import { fetchData } from "../lib/data-fetch.js";
 import { clean } from "../lib/clean.js";
-import { createDynamicRanking } from "../lib/dynamic-ranking";
+import { DynamicRanking } from "../lib/dynamic-ranking";
 
 try {
     // get data for rankings
-    const dataSource = 'https://www.fangraphs.com/api/leaders/major-league/data?age=&pos=all&stats=pit&lg=all&qual=y&season=2024&season1=1871&startdate=&enddate=&month=0&hand=&team=0&pageitems=2000000000&pagenum=1&ind=0&rost=0&players=&type=0&postseason=&sortdir=default&sortstat=SOhttps://statsapi.mlb.com/api/v1/stats?stats=season&group=pitching&season=2024&teamId=136&playerPool=ALL';
+    const dataSource = 'https://statsapi.mlb.com/api/v1/stats?stats=season&group=pitching&season=2024&teamId=136&playerPool=ALL';
     var rawData = await fetchData(dataSource);
 
+
     // manipulate/parse data from file as necessary
-    // rawData = rawData['stats'][0]['splits'];
-    rawData = rawData.data;
+    rawData = rawData['stats'][0]['splits'];
+    // rawData = rawData.data;
+
+    console.log(rawData)
 
     // specify features and their properties
     const features = [
         {
             id: "ERA",
             name: "ERA",
-            path: "ERA",
+            path: "stat:era",
             normalization: 'standard',
             strategy: 'minimize',
             defaultWeight: 0,
@@ -27,7 +30,7 @@ try {
         {
             id: "WHIP",
             name: "WHIP",
-            path: "WHIP",
+            path: "stat:whip",
             normalization: 'log',
             strategy: 'minimize',
             defaultWeight: 1,
@@ -38,7 +41,7 @@ try {
         {
             id: "K9",      // id field cannot contain non-alaphabetic/non-numeric characters
             name: "K/9",
-            path: "K/9",
+            path: "stat:strikeoutsPer9Inn",
             strategy: 'maximize',
             normalization: 'linear',
             defaultWeight: 5,
@@ -53,7 +56,7 @@ try {
     const model = 'weighted-sum';
     const primaryKey = {
         id: "PlayerName",
-        path: "PlayerName"
+        path: "player:fullName"
     }
     const digits = 2;
 
@@ -62,7 +65,7 @@ try {
 
 
     // construct ranking system
-    createDynamicRanking(
+    new DynamicRanking(
         containerId,
         cleanData,
         model,
