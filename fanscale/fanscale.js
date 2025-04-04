08adb38c9592e53778a609a4a314f32a98ed2e7f
shaneferrellwv <shaneferrell@ufl.edu>
2025-04-04 15:13:20 -0400

    fixed float parsing errors in cleaning and separated sliders and rankings window components

--- fanscale/fanscale.js
+++ fanscale/fanscale.js
@@ -2,93 +2,112 @@ import { fetchData } from "../lib/data-fetch.js";
 import { clean } from "../lib/clean.js";
 import { DynamicRanking } from "../lib/dynamic-ranking.js";
 
-try {
-    // craft API URL
-    const baseURL = 
-
-    // get data for rankings
-    const dataSource = 'https://statsapi.mlb.com/api/v1/stats?stats=season&group=pitching&season=2024&playerPool=ALL&limit=853';
-    var rawData = await fetchData(dataSource);
-
-    // manipulate/parse data from file as necessary
-    rawData = rawData['stats'][0]['splits'];
-    // rawData = rawData.data;
-
-    console.log(rawData)
-
-    // specify features and their properties
-    const features = [
-        {
-            id: "ERA",
-            name: "ERA",
-            path: "stat:era",
-            normalization: 'standard',
-            strategy: 'minimize',
-            defaultWeight: 5,
-            userWeight: null,
-            minWeight: 0,
-            maxWeight: 10
-        },
-        {
-            id: "WHIP",
-            name: "WHIP",
-            path: "stat:whip",
-            normalization: 'log',
-            strategy: 'minimize',
-            defaultWeight: 5,
-            userWeight: null,
-            minWeight: 0,
-            maxWeight: 10
-        },
-        {
-            id: "K9",
-            name: "K/9",
-            path: "stat:strikeoutsPer9Inn",
-            strategy: 'maximize',
-            normalization: 'linear',
-            defaultWeight: 5,
-            userWeight: null,
-            minWeight: 0,
-            maxWeight: 10
-        },
-        {
-            id: "BB9",
-            name: "BB/9",
-            path: "stat:walksPer9Inn",
-            strategy: 'minimize',
-            normalization: 'linear',
-            defaultWeight: 5,
-            userWeight: null,
-            minWeight: 0,
-            maxWeight: 10
+export async function createDynamicRanking() {
+    try {
+        // craft API URL
+        const baseURL = document.getElementById('source-select').value;
+        const season = '&season=' + document.getElementById('season-select').value;
+        const group = "&group=" + document.getElementById('group-select').value;
+        const teamID = document.getElementById('team-select').value;
+        var team;
+        if (teamID == 'all') {
+            team = '';
         }
-    ];
+        else {
+            team = '&teamId=' + teamID;
+        }
 
-    // specify container, model, primary key, score decimal places, and ranking sort method
-    const containerId = 'ranking';
-    const model = 'weighted-sum';
-    const primaryKey = {
-        id: "PlayerName",
-        path: "player:fullName"
+        // get data for rankings
+        const dataSource = baseURL + group + season + team
+                        + '&playerPool=ALL&limit=999';
+        
+        var rawData = await fetchData(dataSource);
+
+        // manipulate/parse data from file as necessary
+        rawData = rawData['stats'][0]['splits'];
+        // rawData = rawData.data;
+
+        // console.log(rawData)
+
+        // specify features and their properties
+        const features = [
+            {
+                id: "ERA",
+                name: "ERA",
+                path: "stat:era",
+                normalization: 'standard',
+                strategy: 'minimize',
+                defaultWeight: 5,
+                userWeight: null,
+                minWeight: 0,
+                maxWeight: 10
+            },
+            {
+                id: "WHIP",
+                name: "WHIP",
+                path: "stat:whip",
+                normalization: 'log',
+                strategy: 'minimize',
+                defaultWeight: 5,
+                userWeight: null,
+                minWeight: 0,
+                maxWeight: 10
+            },
+            {
+                id: "K9",
+                name: "K/9",
+                path: "stat:strikeoutsPer9Inn",
+                strategy: 'maximize',
+                normalization: 'linear',
+                defaultWeight: 5,
+                userWeight: null,
+                minWeight: 0,
+                maxWeight: 10
+            },
+            {
+                id: "BB9",
+                name: "BB/9",
+                path: "stat:walksPer9Inn",
+                strategy: 'minimize',
+                normalization: 'linear',
+                defaultWeight: 5,
+                userWeight: null,
+                minWeight: 0,
+                maxWeight: 10
+            }
+        ];
+
+        // specify container, model, primary key, score decimal places, and ranking sort method
+        // const containerId = 'ranking';
+        const slidersContainerId = 'sliders-container';
+        const rankingsContainerId = 'rankings-container';
+        const model = 'weighted-sum';
+        const primaryKey = {
+            id: "PlayerName",
+            path: "player:fullName"
+        }
+        const digits = 2;
+        const sort = "descending";
+
+        // clean data
+        const cleanData = clean(rawData, features, primaryKey);
+
+        console.log(cleanData)
+
+        // construct ranking system
+        new DynamicRanking(
+            slidersContainerId,
+            rankingsContainerId,
+            cleanData,
+            model,
+            features,
+            primaryKey,
+            digits,
+            sort
+        );
     }
-    const digits = 2;
-    const sort = "descending";
 
-    // clean data
-    const cleanData = clean(rawData, features, primaryKey);
-
-    // construct ranking system
-    new DynamicRanking(
-        containerId,
-        cleanData,
-        model,
-        features,
-        primaryKey,
-        digits,
-        sort
-    );
-}
-
-catch (error) {
-    console.error(error);
+    catch (error) {
+        console.error(error);
+    }
 }
--- fanscale/index.js
+++ fanscale/index.js
@@ -1,7 +1,12 @@
+import { createDynamicRanking } from "./fanscale";
+
 function fetchTeams() {
     const teamSelect = document.getElementById('team-select');
+    teamSelect.innerHTML = '';
   
-    fetch('https://statsapi.mlb.com/api/v1/teams?sportId=1')
+    const seasonParam = document.getElementById('season-select').value;
+  
+    fetch('https://statsapi.mlb.com/api/v1/teams?sportId=1&season=' + seasonParam)
       .then(res => res.json())
       .then(data => {
         const allOption = document.createElement('option');
@@ -18,11 +23,44 @@ function fetchTeams() {
       });
 }
 
-try {
+function fetchSeasons() {
+  const seasonSelect = document.getElementById('season-select');
+
+  for (let year = 2025; year >= 1876; year--) {
+    const option = document.createElement('option');
+    option.value = year;
+    option.textContent = year;
+    seasonSelect.appendChild(option);
+  }
+}
+
+function addSeasonSelectionListener() {
+  document.getElementById('season-select').addEventListener('change', function () {
     fetchTeams();
+    createDynamicRanking();
+  });
+}
+
+function addTeamSelectionListener() {
+  document.getElementById('team-select').addEventListener('change', function () {
+    createDynamicRanking();
+  });
+}
+
+function addGroupSelectionListener() {
+  document.getElementById('group-select').addEventListener('change', function () {
+    createDynamicRanking();
+  });
+}
+
+try {
+  addSeasonSelectionListener();
+  addTeamSelectionListener();
+  addGroupSelectionListener();
+  fetchSeasons();
+  fetchTeams();
 }
 
 catch (error) {
     console.error(error);
 }
-
